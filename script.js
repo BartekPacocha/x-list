3a31f372f7013b3dff96f566879c0583a20d4af8
BartekPacocha <bartekpacocha@gmail.com>
2017-10-11 22:43:07 +0200

    final commit

--- script.js
+++ script.js
@@ -1,76 +1,11 @@
 var app = angular.module("myapp",[]);
 app.controller("ListController", ['$scope', function($scope) {
-	$scope.data = [
-		{
-			'firstName':'Jan',
-			'lastName':'Kowalski',
-			'position':'Admin',
-			'role':'Admin',
-			'experience':'5',
-			'shortDescription':'Jan Kowalski short',
-			'longDescription':'Jan Kowalski long',
-			'id':'1'
-		},
-		{
-			'firstName':'Adam',
-			'lastName':'Nowak',
-			'position':'HR',
-			'role':'User',
-			'experience':'2',
-			'shortDescription':'Adam Nowak short',
-			'longDescription':'Adam Nowak long',
-			'id':'2'
-		},
-		{
-			'firstName':'Janina',
-			'lastName':'Jajko',
-			'position':'HR',
-			'role':'User',
-			'experience':'3',
-			'shortDescription':'Janina Jajko short',
-			'longDescription':'Janina Jajko long',
-			'id':'3'
-		}
-	];
-
-	$scope.editingData = {};
-
-	for(var i = 0, lenght = $scope.data.lenght; i < lenght; i++) {
-		$scope.editingData[$scope.data[i].id] = false;
-	}
-
-	$scope.modify = function(data) {
-		$scope.editingData[data.id] = true;
-	}
-
-	$scope.update = function(data){
-        $scope.editingData[data.id] = false;
+	
+	
+    $scope.delete = function(){
+    	const id = $scope.editetIndex;
+    	$scope.data.splice(id, 1);
     };
-
-    $scope.delete = function(x){
-    	$scope.data.splice(x, 1);
-    };
-
-    $scope.add = function(){
-    	var newElement = {
-    		'firstName':$scope.addNew_firstName,
-    		'lastName':$scope.addNew_lastName,
-    		'position':$scope.addNew_position,
-    		'role':$scope.addNew_role,
-    		'experience':$scope.addNew_experience,
-    		'shortDescription':$scope.addNew_shortDescription,
-    		'longDescription':$scope.addNew_longDescription
-
-    	};
-    	$scope.data.push(newElement);
-    	$scope.addNew_firstName = '';
-    	$scope.addNew_lastName = '';
-    	$scope.addNew_position = '';
-    	$scope.addNew_role = '';
-    	$scope.addNew_experience = '';
-    	$scope.addNew_shortDescription = '';
-    	$scope.addNew_longDescription = '';
-    }
 
 	$scope.popId = null;
 
@@ -84,8 +19,122 @@ app.controller("ListController", ['$scope', function($scope) {
 		$scope.popId = null; 
 	};
 
-	$scope.fun = function(data) {
-		alert(data.lastName);
+	$scope.onclickRow = function(data, index) {
+		$scope.editetIndex = index;
+		$scope.editedElement = data;
+		$scope.experience = data.experience;
+		$scope.shortDescription = data.shortDescription;
+		$scope.longDescription = data.longDescription;
 	}
+
+	$scope.update = function() {
+		const id = $scope.editetIndex;
+		$scope.data[id].experience = $scope.experience;
+		$scope.data[id].shortDescription = $scope.shortDescription;
+		$scope.data[id].longDescription = $scope.longDescription;
+	}
+
+	$scope.data = [
+		{
+			'firstName':'Jan',
+			'lastName':'Kowalski',
+			'position':'Admin',
+			'role':'Admin',
+			'experience':5,
+			'shortDescription':'description short jan',
+			'longDescription':'description long jan',
+			'id':'0'
+		},
+		{
+			'firstName':'Adam',
+			'lastName':'Nowak',
+			'position':'HR',
+			'role':'User',
+			'experience':2,
+			'shortDescription':'description short adam',
+			'longDescription':'description long adam',
+			'id':'1'
+		},
+		{
+			'firstName':'Kacper',
+			'lastName':'Ciesielski',
+			'position':'HR',
+			'role':'User',
+			'experience':53,
+			'shortDescription':'description short kacper',
+			'longDescription':'description long kacper',
+			'id':'2'
+		},
+		{
+			'firstName':'Krzysztof',
+			'lastName':'Polak',
+			'position':'Developer,',
+			'role':'User',
+			'experience':34,
+			'shortDescription':'description short krzysztof',
+			'longDescription':'description long krzysztof',
+			'id':'3'
+		},
+		{
+			'firstName':'Magdalena',
+			'lastName':'Wilk',
+			'position':'Admin',
+			'role':'Admin',
+			'experience':54,
+			'shortDescription':'description short magdalena',
+			'longDescription':'description long magdalena',
+			'id':'0'
+		},
+		{
+			'firstName':'Liliana',
+			'lastName':'Zając',
+			'position':'Developer,',
+			'role':'User',
+			'experience':2,
+			'shortDescription':'description short liliana',
+			'longDescription':'description long liliana',
+			'id':'1'
+		},
+		{
+			'firstName':'Jakub',
+			'lastName':'Cybulski',
+			'position':'Tester,',
+			'role':'User',
+			'experience':65,
+			'shortDescription':'description short jakub',
+			'longDescription':'description long jakub',
+			'id':'2'
+		},
+		{
+			'firstName':'Janina',
+			'lastName':'Jajko2',
+			'position':'Developer,',
+			'role':'Admin',
+			'experience':23,
+			'shortDescription':'description short janina',
+			'longDescription':'description long janina',
+			'id':'3'
+		},
+		{
+			'firstName':'Michał',
+			'lastName':'Urban',
+			'position':'Developer,',
+			'role':'User',
+			'experience':42,
+			'shortDescription':'description short michal',
+			'longDescription':'description long michal',
+			'id':'2'
+		},
+		{
+			'firstName':'Patrycja',
+			'lastName':'Piotrowska',
+			'position':'Manager,',
+			'role':'User',
+			'experience':78,
+			'shortDescription':'description short patrycja',
+			'longDescription':'description long patrycja',
+			'id':'3'
+		}
+	];
 
 }]);
